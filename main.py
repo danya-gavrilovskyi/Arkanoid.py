823679121e0f485054f22320e864628e89cf903e
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2023-01-18 21:49:21 +0200

    Added imports

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import pgzrun
+import random
+from pgzero.actor import Actor
 
 class Paddle():
-    pass
+    def __init__(self, x, y, height, width):
+        self.x = x
+        self.y = y
+        self.height = height
+        self.width = width
+
+    def draw(self):
+        rect = Rect(self.x, self.y, self.width, self.height)
+        screen.draw.filled_rect(rect, 'black')
 
 class Ball():
     pass
@@ -14,5 +24,9 @@ def draw():
 
 WIDTH = 600
 HEIGHT = 600
+
+paddle_h = 35
+paddle_w = 200
+paddle = Paddle(WIDTH//2 - (paddle_w//2), HEIGHT - paddle_h, paddle_h, paddle_w)
 
 pgzrun.go()
